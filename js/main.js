54586a983eba3db04310033fa9c9a6cfd29d7d1b
jose carlos <castillo.villa@nube.unadmexico.mx>
2021-08-02 02:28:02 -0500

    mostrar ocultar menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function Menu() {
+    var x = document.getElementById("lista");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
